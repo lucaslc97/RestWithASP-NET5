b47e5ef63b29f53bd501b090571d2856283027de
Lucas Cabral <49406931+lucaslc97@users.noreply.github.com>
2021-10-24 21:19:51 -0300

    Calculadora

---
+++ 02_RestWithASPNET5_Calculadora/RestWithASPNET5/RestWithASPNET5/Controllers/CalculatorController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firtNumber}/{secondNumber}")]
+        public IActionResult Get(string firtNumber, string secondNumber)
+        {
+
+            if(IsNumeric(firtNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firtNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber,
+                System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+            return isNumber;
+        }
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+                return 0;
+        }
+
+    }
+}
