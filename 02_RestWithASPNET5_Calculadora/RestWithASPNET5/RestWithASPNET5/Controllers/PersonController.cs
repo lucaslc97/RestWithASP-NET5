cc53f2723e39be37a7be71479c19b62730a341ac
Lucas Cabral <49406931+lucaslc97@users.noreply.github.com>
2021-10-25 20:23:22 -0300

    Trabalhando com outros verbos.

---
+++ 02_RestWithASPNET5_Calculadora/RestWithASPNET5/RestWithASPNET5/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firtNumber}/{secondNumber}")]
+        public IActionResult Sum(string firtNumber, string secondNumber)
+        {
+
+            
+            return BadRequest("Invalid Input");
+        }
+
+       
+    }
+}
