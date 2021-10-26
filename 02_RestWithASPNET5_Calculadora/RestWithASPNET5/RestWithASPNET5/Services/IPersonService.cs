a8898cee3d369a2670e9f84f95d107585504a347
Lucas Cabral <49406931+lucaslc97@users.noreply.github.com>
2021-10-25 21:11:35 -0300

    Conhecendo os verbos GET, POST, PUT E DELETE

---
+++ 02_RestWithASPNET5_Calculadora/RestWithASPNET5/RestWithASPNET5/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNET5.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindbyId(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        Person Delete(long id);
+
+    }
+}
---
+++ 02_RestWithASPNET5_Calculadora/RestWithASPNET5/RestWithASPNET5/Services/Implementations/PersonServicesImplementations.cs
@@ -0,0 +1,70 @@
+﻿using RestWithASPNET5.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5.Services.Implementations
+{
+    public class PersonServicesImplementations : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public Person Delete(long id)
+        {
+            return null;
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            };
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirtName = "Person Name" + i,
+                LastName = "Person Last name" + i,
+                Adress = "Some adress" + i,
+                Gender = "Male"
+            };
+        }
+
+        public Person FindbyId(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirtName = "Lucas",
+                LastName = "Cabral",
+                Adress = "Duque de Caxias/RJ",
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
